141c1313f76bc19e2c7b036cd051ddb7e1a7de4c
rejaul karim <rejaulkarim@rejauls-MacBook-Air.local>
2025-01-07 11:07:56 +0600

    9th commit

--- src/app.jsx
+++ src/app.jsx
@@ -8,7 +8,7 @@ export function App() {
 
   return (
     <>
-      <h1>Hello World</h1>
+      <h1>Hello World 1</h1>
     </>
   )
 }
--- test/App.test.js
+++ test/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/preact';
-import App from '../app';
+import App from '/src/App';
 
 test('renders hello world', () => {
   render(<App />);
